94bc4ea7725ec0f24ad73c276d7addd7805efc9f
Dave Tarc <davetarc@gmail.com>
2013-08-18 19:38:47 -0700

    Adding static description field

--- StrategyField.php
+++ StrategyField.php
@@ -4,7 +4,8 @@ interface StrategyField {
   public function __construct($strategy_values = NULL);
 
   public static function getLabel();  
-
+  public static function getDescription();
+  
   public function formatter($display);
   public function getForm();
   public function validate();
